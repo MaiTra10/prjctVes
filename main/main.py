45e713840d4d8b931d65c6ede304b3cf55c6f73f
MaiTra-P <55607989+MaiTra10@users.noreply.github.com>
2023-07-04 19:31:53 -0600

    msc
    removed unnecessary comments

--- main/main.py
+++ main/main.py
@@ -2,16 +2,20 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-# OS import for loading environment variable
 import os
-# get token from environment variables
-TOKEN = os.getenv('DISCORD_TOKEN_VES')
-# initialize bot with '.' prefix and feault intents
+from twelvedata import TDClient
+from typing import Optional
+
+TOKEN = os.getenv("DISCORD_TOKEN_VES")
+API_KEY = os.getenv("API_KEY_TD")
+
+td = TDClient(apikey = API_KEY)
+
 bot = commands.Bot(command_prefix = ".", intents = discord.Intents.default())
-# Initial bot event
+
 @bot.event
 async def on_ready():
-    # Try to sync slash commands, catch Exception and print it
+
     try:
         
         synced = await bot.tree.sync()
@@ -22,10 +26,16 @@ async def on_ready():
         print(e)
 
     print(f"{bot.user.name} is cooking!")
+
 # Slash Command: /say [say_this]
 @bot.tree.command(name = "say", description = "Tell Ves to say something.")
 @app_commands.describe(say_this = "What do you want me to say?")
-async def say(interaction: discord.Interaction, say_this: str):
-    await interaction.response.send_message(f"{interaction.user.mention} told me to say: ```{say_this}```") # Add 'ephemeral = True' to make it visible only to the user
-# Run bot using unique token
+async def say(interaction: discord.Interaction, say_this: Optional[str] = None):
+
+    await interaction.response.send_message(f"{interaction.user.mention} told me to say: '**{say_this}**'") # Add 'ephemeral = True' to make it visible only to the user
+
+@bot.tree.command(name = "watchlist", description = "Interact with your stocks watchlist.")
+@app_commands.describe(action = "Add | add a stock to your watchlist, Remove | remove a stock from your watchlist, Choose | get a specific stock from your watchlist")
+
+
 bot.run(TOKEN)
