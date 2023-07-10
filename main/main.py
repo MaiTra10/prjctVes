08595a120bd22c603c19d800b8c865320d412c18
MaiTra-P <55607989+MaiTra10@users.noreply.github.com>
2023-07-10 10:21:48 -0600

    feat
    added function to call the prjctVes API

--- main/main.py
+++ main/main.py
@@ -1,17 +1,39 @@
 # Ves
 import discord
+import os
+import requests
+import json
 from discord import app_commands
 from discord.ext import commands
-import os
-from twelvedata import TDClient
 from typing import Optional
 
 TOKEN = os.getenv("DISCORD_TOKEN_VES")
 API_KEY = os.getenv("API_KEY_TD")
 
-td = TDClient(apikey = API_KEY)
-
 bot = commands.Bot(command_prefix = ".", intents = discord.Intents.default())
+
+def api(http_method, method, params):
+
+    url = "https://4qq4mnhpug.execute-api.us-west-2.amazonaws.com/prod/" + method
+
+    if http_method == "POST":
+
+        resp = requests.post(url, params = params)
+
+    elif http_method == "GET":
+
+        resp = requests.get(url, params = params)
+
+    else:
+
+        resp = requests.delete(url, params = params)
+
+    return {
+
+        "StatusCode": resp.status_code,
+        "body": resp.text
+
+    }
 
 @bot.event
 async def on_ready():
@@ -34,8 +56,10 @@ async def say(interaction: discord.Interaction, say_this: Optional[str] = None):
 
     await interaction.response.send_message(f"{interaction.user.mention} told me to say: '**{say_this}**'") # Add 'ephemeral = True' to make it visible only to the user
 
-@bot.tree.command(name = "watchlist", description = "Interact with your stocks watchlist.")
-@app_commands.describe(action = "Add | add a stock to your watchlist, Remove | remove a stock from your watchlist, Choose | get a specific stock from your watchlist")
+@bot.tree.command(name = "wl_add", description = "Add an entry to your watchlist")
+@app_commands.describe(choice = "Choose either 'steam' or 'stock'", name = "Steam: Exact name of item and wear (if any) at end in brackets | Stock: '[ticker]:[exchange]'")
+async def wl_add(interaction: discord.Interaction, choice: str, name:str):
 
+    await interaction.response.send_message(f"User ID: {interaction.user.id}\nChoice: {choice}\nName: {name}")
 
 bot.run(TOKEN)
